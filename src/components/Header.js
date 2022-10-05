2b18106f7dae3444a3c43956ec0adbf769d5709a
Larissa Perinoto <perinotolarissa@gmail.com>
2022-10-05 09:40:47 -0300

    Cria o componente Header.

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header ({ characters }) {
+  return (
+    <header className="character-container">
+       <h1>Rick and Morty and HOFs</h1>
+    </header>
+  )
+}
